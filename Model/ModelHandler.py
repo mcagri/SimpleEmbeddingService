d7751ae7f24b7a2bd491718f2173b397f274cfbf
mcc <mehmet.cagri.calpur@gmail.com>
2024-01-14 18:24:12 +0300

    Minor changes, clean up scaffolding code.

--- Model/ModelHandler.py
+++ Model/ModelHandler.py
@@ -1,7 +1,10 @@
 import torch.nn.functional as F
-
+import torch
 from torch import Tensor
 from transformers import AutoTokenizer, AutoModel
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
 
 def average_pool(last_hidden_states: Tensor,
@@ -10,12 +13,13 @@ def average_pool(last_hidden_states: Tensor,
     return last_hidden.sum(dim=1) / attention_mask.sum(dim=1)[..., None]
 
 
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 tokenizer = AutoTokenizer.from_pretrained('intfloat/multilingual-e5-large')
-model = AutoModel.from_pretrained('intfloat/multilingual-e5-large')
+model = AutoModel.from_pretrained('intfloat/multilingual-e5-large').to(device)
 
 
 def get_embedding(text: str):
-    batch_dict = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt')
+    batch_dict = tokenizer(text, max_length=512, padding=True, truncation=True, return_tensors='pt').to(device)
     outputs = model(**batch_dict)
     embedding = F.normalize(average_pool(outputs.last_hidden_state, batch_dict['attention_mask'])).tolist()[0]
     return embedding
--- main.py
+++ main.py
@@ -3,16 +3,6 @@ from Model import ModelHandler
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
 @app.post("/embedtext/")
 async def get_text_embedding(obj: dict,):
     try:
